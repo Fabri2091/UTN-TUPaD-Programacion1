34cf40c92ddf8d60faffb5bbc843713089d323ad
Fabri2091 <provati2091@gmail.com>
2025-10-18 20:25:51 -0300

    TP5

--- Actividades Listas.py
+++ Actividades Listas.py
@@ -124,11 +124,13 @@
 # ACTIVIDAD 10
 
 
-# lista_anidada = [[]] * 4      
-# lista_anidada[0] = 15
-# lista_anidada[1] = True
-# lista_anidada[2] = [25.5, 57.9, 30.6]   
-# lista_anidada[3] = False
 
-# print(lista_anidada)
+
+lista_anidada = [[]] * 4      
+lista_anidada[0] = 15
+lista_anidada[1] = True
+lista_anidada[2] = [25.5, 57.9, 30.6]   
+lista_anidada[3] = False
+
+print(lista_anidada)
 
