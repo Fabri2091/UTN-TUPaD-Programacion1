87d575fcb9cc28e2514a842dc86120d6af57b80b
Fabri2091 <provati2091@gmail.com>
2025-10-18 19:56:43 -0300

    Practico 5

--- Actividades Listas.py
+++ Actividades Listas.py
@@ -1,3 +1,136 @@
 
 
-print ("Hola, Mundo!")
+# ACTIVIDAD 1
+# lista_de_numeros = list(range(4, 101, 4))
+
+# print(lista_de_numeros)
+
+
+
+# ACTIVIDAD 2
+# Lista = list(["Hola", "Mundo", 123, 45, True])
+
+
+
+# print(Lista[-2])
+
+
+
+# ACTIVIDAD 3
+
+# Lista_vacia = []
+
+
+# print(Lista_vacia)
+
+# Lista_completa = Lista_vacia.append(1)
+# Lista_completa = Lista_vacia.append(2)
+# Lista_completa = Lista_vacia.append(3)
+
+
+# print(Lista_vacia)
+
+
+# ACTIVIDAD 4
+
+# animales = ["perro", "gato", "conejo", "pez"]
+
+# print(animales)
+
+
+# animales.pop(1)
+# animales.pop(-1)
+# animales.insert(1, "loro")
+# animales.append("oso")
+
+
+# print(animales)
+
+
+
+
+# ACTIVIDAD 5
+
+# numeros =[8, 15, 3, 22, 7]
+
+# numeros.remove(max(numeros))
+# print(numeros)
+
+
+#Este ejercicio elimina el numero mas grande de la lista
+
+
+
+
+# ACTIVIDAD 6
+
+# Lista = list(range(10, 31, 5))
+
+# print(Lista)
+
+# print(f"{Lista[0]}, {Lista[1]}")
+
+
+
+
+# ACTIVIDAD 7
+
+# autos = ["sedan", "polo", "suran", "gol"]
+
+# print(autos)
+
+# autos.pop(0)
+# autos.pop(0)
+
+# autos.insert(0, "Fennec")
+# autos.insert(1, "Mustang")
+
+# print(autos)
+
+
+
+# ACTIVIDAD 8
+
+# dobles = []
+
+# dobles.append(5*2)
+# dobles.append(10*2)
+# dobles.append(15*2)
+
+# print (dobles)
+
+
+
+# ACTIVIDAD 9
+# compras = [
+#     ["pan", "Leche"],
+#     ["arroz", "fideos", "salsa"],
+#     ["agua"],
+# ]
+
+# print(compras)
+
+# compras[-1].append("jugo")
+
+# compras[1].remove("fideos")
+# compras[1].insert(1, "tallarines")
+
+# compras[0].remove("pan")
+
+# print(compras)
+
+
+
+# ACTIVIDAD 10
+
+
+
+
+lista_anidada = [[]] * 4      
+lista_anidada[0] = 15
+lista_anidada[1] = True
+lista_anidada[2] = [25.5, 57.9, 30.6]   
+lista_anidada[3] = False
+
+print(lista_anidada)
+
